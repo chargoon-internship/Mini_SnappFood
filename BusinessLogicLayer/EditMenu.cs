e3a2395a7a9999361ccd3af9976047eea767cd8b
mohaddesse <mohaddesseAghasizade@gmail.com>
2023-06-02 14:09:57 -0700

    Edit Reposotory And Add page for edit menu

---
+++ BusinessLogicLayer/EditMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayes;
+using Entities;
+
+namespace BusinessLogicLayer
+{
+    public class EditMenu
+    {
+        UnitOfWork db = new UnitOfWork();
+
+        public List<Food> PrintAllMenu(int id)
+        {
+           var foods=db.FoodRepository.GetAll();
+            return foods.Where(f=>f.Restaurant_Id==id).ToList();
+        }
+        public void AddMenu(Food food)
+        {
+            db.FoodRepository.Insert(food);
+            
+        }
+        public void UpdateMenu(Food food)
+        {
+            db.FoodRepository.Update(food);
+        }
+        public Food FindMenuById(int id)
+        {
+           return db.FoodRepository.GetById(id);
+        }
+    }
+}
--- DataAccessLayes/Migrations/20230602170850_mogration2.Designer.cs
+++ DataAccessLayes/Migrations/20230602170850_mogration2.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayes.Migrations
 {
     [DbContext(typeof(DB))]
-    partial class DBModelSnapshot : ModelSnapshot
+    [Migration("20230602170850_mogration2")]
+    partial class mogration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
