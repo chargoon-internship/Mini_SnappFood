467eacd62e440409e677611544b56a19baacafea
Reyhaneh <khalili.reyhaneh2001@gmail.com>
2023-06-04 14:09:18 +0330

    - Design Restaurant Panel & Restaurant Invoice UI

---
+++ SnappFood/RestaurantInvoice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnappFood
+{
+    public partial class RestaurantInvoice : Form
+    {
+        public RestaurantInvoice()
+        {
+            InitializeComponent();
+        }
+
+        private void RestaurantInvoice_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SnappFood/RestaurantPanel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnappFood
+{
+    public partial class RestaurantPanel : Form
+    {
+        public RestaurantPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void RestaurantPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegisterMenu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditMenu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnViewInvoices_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
