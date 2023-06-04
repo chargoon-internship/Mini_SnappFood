7e0a5cc630de8ab3735c44e3c7e48516e73bd0a0
Reyhaneh <khalili.reyhaneh2001@gmail.com>
2023-06-04 10:54:50 +0330

    - Design Restaurant Panel & Restaurant Invoice UI

---
+++ SnappFood/RestaurantPanel.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SnappFood
+{
+    partial class RestaurantPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRegisterMenu = new Button();
+            btnEditMenu = new Button();
+            btnViewInvoices = new Button();
+            SuspendLayout();
+            // 
+            // btnRegisterMenu
+            // 
+            btnRegisterMenu.Font = new Font("IRANSansWeb(FaNum)", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            btnRegisterMenu.Location = new Point(522, 86);
+            btnRegisterMenu.Margin = new Padding(3, 4, 3, 4);
+            btnRegisterMenu.Name = "btnRegisterMenu";
+            btnRegisterMenu.Size = new Size(314, 134);
+            btnRegisterMenu.TabIndex = 0;
+            btnRegisterMenu.Text = "ثبت منوی جدید";
+            btnRegisterMenu.UseVisualStyleBackColor = true;
+            btnRegisterMenu.Click += btnRegisterMenu_Click;
+            // 
+            // btnEditMenu
+            // 
+            btnEditMenu.Font = new Font("IRANSansWeb(FaNum)", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEditMenu.Location = new Point(522, 282);
+            btnEditMenu.Name = "btnEditMenu";
+            btnEditMenu.Size = new Size(314, 127);
+            btnEditMenu.TabIndex = 1;
+            btnEditMenu.Text = "ویرایش منو";
+            btnEditMenu.UseVisualStyleBackColor = true;
+            btnEditMenu.Click += btnEditMenu_Click;
+            // 
+            // btnViewInvoices
+            // 
+            btnViewInvoices.Font = new Font("IRANSansWeb(FaNum)", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            btnViewInvoices.Location = new Point(522, 462);
+            btnViewInvoices.Name = "btnViewInvoices";
+            btnViewInvoices.Size = new Size(314, 117);
+            btnViewInvoices.TabIndex = 2;
+            btnViewInvoices.Text = "مشاهده فاکتورها";
+            btnViewInvoices.UseVisualStyleBackColor = true;
+            btnViewInvoices.Click += btnViewInvoices_Click;
+            // 
+            // RestaurantPanel
+            // 
+            AutoScaleDimensions = new SizeF(8F, 22F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(217, 0, 79);
+            ClientSize = new Size(1400, 800);
+            Controls.Add(btnViewInvoices);
+            Controls.Add(btnEditMenu);
+            Controls.Add(btnRegisterMenu);
+            Font = new Font("IRANSansWeb(FaNum)", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "RestaurantPanel";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "پنل رستوران";
+            Load += RestaurantPanel_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnRegisterMenu;
+        private Button btnEditMenu;
+        private Button btnViewInvoices;
+    }
+}
