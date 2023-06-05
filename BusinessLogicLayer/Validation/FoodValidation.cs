c5a4130bc04a5d727b2589b3f901137fa995be53
mohaddesse <mohaddesseAghasizade@gmail.com>
2023-06-05 02:25:50 -0700

    Add Validation for restaurant

---
+++ BusinessLogicLayer/Validation/FoodValidation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using System.Text.RegularExpressions;
+using BusinessLogicLayer.Enum;
+using DataAccessLayes;
+
+namespace BusinessLogicLayer
+{
+    public static class FoodValidation
+    {
+        private static string MainValidation(Food food)
+        {
+      
+            var NameOfFood=food.Name;
+            var PriceOfFood = food.Price;
+            bool chRegx= CheckRegex("Name",NameOfFood);
+
+
+            if (NameOfFood == "" || PriceOfFood == 0)
+                return "لطفا تمام گزینه ها را پر کنید.";
+            else if (!chRegx)
+                return "نام غذا ترکیبی از حروف می باشد.";
+            else return "موفقیت";
+        }
+        public static string EditValidation(Food food)
+        {
+            bool uniqFood;
+            string result = MainValidation(food);
+            using (UnitOfWork db = new UnitOfWork())
+            {
+                uniqFood = db.FoodRepository.IsExistUpdate(food);
+            }
+            if (uniqFood)
+                return "نام غذا تکراری است";
+            return result;
+        }
+        public static string AddValidation(Food food)
+        {
+            bool uniqFood;
+            string result= MainValidation(food);
+            using (UnitOfWork db = new UnitOfWork())
+            {
+                uniqFood = db.FoodRepository.IsExistFood(food.Name);
+            }
+            if (uniqFood)
+                return "نام غذا تکراری است";
+            return result;
+        }
+        
+        private static bool CheckRegex(string pattern,string target)
+        {
+            Regex rg = null;
+            switch (pattern)
+            {
+                case "Name":
+                    rg = new Regex(RegexConstants.PatternName);
+                    break;
+                case "Address":
+                    rg = new Regex(RegexConstants.Address);
+                    break;
+                case "NationalCode":
+                    rg = new Regex(RegexConstants.NationalCode);
+                    break;
+            }
+
+            if (rg != null)
+            {
+                return rg.IsMatch(target);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
--- SnappFood/EditandAddMenu.cs
+++ SnappFood/EditandAddMenu.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using BusinessLogicLayer;
 
 using Entities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Model;
 
 namespace SnappFood
 {
@@ -25,10 +26,16 @@ namespace SnappFood
             InitializeComponent();
             if (isEdit)
             {
-                btnDeletMenu.Visible = true;
-                btnEditMenu.Visible = true;
-                btnSubmit.Text = "ویرایش";
+                ShowButtonForEdit();
             }
+        }
+
+        private void ShowButtonForEdit()
+        {
+            btnDeletMenu.Visible = true;
+            btnEditMenu.Visible = true;
+            btnSubmit.Visible = false;
+            btnSubmit.Text = "ویرایش";
         }
 
         private void EditandAddMenu_Load(object sender, EventArgs e)
@@ -54,49 +61,70 @@ namespace SnappFood
         {
             var name = txtFood.Text;
             var price = txtPrice.Text.Replace("ریال", "").Replace(",", "").Replace("\"", "").Trim();
+
             double.TryParse(price, out double priceOfFood);
             bool isExist = chBoxExistence.Checked;
             Food food;
             if (btnSubmit.Text == "ویرایش")
             {
-                food = new Food { Id = int.Parse(txtID.Text), Price = priceOfFood, Name = name, Restaurant_Id = 2, Exist = isExist };
-                var result = menu.UpdateMenu(food);
+                var IdOfFood = int.Parse(txtID.Text);
+                List<string> result = new List<string>();
 
+                    food = new Food { Id = IdOfFood, Price = priceOfFood, Name = name, Restaurant_Id = 2, Exist = isExist };
+                    result = menu.UpdateMenu(food);
+                    
+                if (result[0] != "موفقیت")
+                {
+                    MessageBox.Show(result[0], "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    UpdateFood(IdOfFood);
+                }
+                else
+                {
+                    MessageBox.Show(result[1], "اعلام", MessageBoxButtons.OK);
+                    ShowButtonForEdit();
+                    SetEmpty();
 
-                MessageBox.Show(result, "اعلام", MessageBoxButtons.OK);
-
-
+                }
             }
             else
             {
                 
                 food = new Food { Price = priceOfFood, Name = name, Restaurant_Id = 2, Exist = isExist };
+                string result=menu.AddMenu(food);
+                if (result!= "موفقیت")
+                {
+                    MessageBox.Show(result, "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
-                menu.AddMenu(food);
+                }
+                else
+                {
+                    SetEmpty();
+                }
             }
             BindGrid();
-            SetEmpty();
+  
         }
-
         private void btnEditMenu_Click(object sender, EventArgs e)
         {
             if (dgMenu.CurrentRow != null)
             {
                 int IdOfFood = (int)dgMenu.CurrentRow.Cells[0].Value;
-
-                var food = menu.FindMenuById(IdOfFood);
-                txtPrice.Text = food.Price.ToString();
-                txtFood.Text = food.Name.ToString();
-                chBoxExistence.Checked = food.Exist;
-                txtID.Text = IdOfFood.ToString();
-
-
-
+                UpdateFood(IdOfFood);
+                btnSubmit.Visible = true;
             }
 
             BindGrid();
         }
 
+        private void UpdateFood(int IdOfFood)
+        {
+            var food = menu.FindMenuById(IdOfFood);
+            txtPrice.Text = food.Price.ToString();
+            txtFood.Text = food.Name.ToString();
+            chBoxExistence.Checked = food.Exist;
+            txtID.Text = IdOfFood.ToString();
+            
+        }
         private void btnDeletMenu_Click(object sender, EventArgs e)
         {
             if (dgMenu.CurrentRow != null)
@@ -115,7 +143,6 @@ namespace SnappFood
             }
 
         }
-
         private void btnReturn_Click(object sender, EventArgs e)
         {
             this.Close();
