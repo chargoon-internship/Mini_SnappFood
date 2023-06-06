397ef86a3604ac795087ec538cdbbca8255b13bd
bahar <baharoruji2@gmail.com>
2023-06-06 11:15:23 +0330

    adding customer panel service

---
+++ ViewModel/Rertaurant/RestaurantDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ViewModel.Rertaurant
+{
+    public class RestaurantDto
+    {
+        public string RestaurantName { get; set; }
+/*
+        public string RestaurantType { get; set; }
+
+        public string RestaurantScore { get; set; }*/
+
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            /*return $"{RestaurantName}({Address})\n ({RestaurantScore})\n{RestaurantType}";*/
+            return $"{RestaurantName}({Address})\n";
+        }
+    }
+}
