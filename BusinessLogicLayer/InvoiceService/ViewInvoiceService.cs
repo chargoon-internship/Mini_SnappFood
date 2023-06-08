a7b92f8eadc0b890120f159d8d8aab439c1f1cc8
Reyhaneh <khalili.reyhaneh2001@gmail.com>
2023-06-09 01:44:35 +0330

    Delete PrintCustomerInvoices method

--- BusinessLogicLayer/InvoiceService/ViewInvoiceService.cs
+++ BusinessLogicLayer/InvoiceService/ViewInvoiceService.cs
@@ -17,16 +17,6 @@ namespace BusinessLogicLayer.InvoiceService
                 var invoices = db.InvoiceRepository.GetRestaurantInvoices(id);
                 return invoices;
             }
-
-        }
-
-        public List<Invoice> PrintCustomerInvoices(int id)
-        {
-            using (UnitOfWork db=new UnitOfWork())
-            {
-                var invoices = db.InvoiceRepository.GetCustomerInvoice(id);
-                return invoices;
-            }
         }
     }
 }
