47a5e7498a23a4fffafc435e694989357a030454
alireza1379sa <alireza1379ssa@gmail.com>
2023-06-10 02:18:31 +0330

    add converter

---
+++ ViewModel/Invoice/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Invoice
+{
+    public static class Converter
+    {
+        public static string ToShamsi(this DateTime time)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            return persianCalendar.GetYear(time) + "/" + persianCalendar.GetMonth(time) + "/" + persianCalendar.GetDayOfMonth(time);
+        }
+    }
+}
